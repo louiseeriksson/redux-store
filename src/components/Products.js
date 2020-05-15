aa099d6d8547495f61b77f602ca7a984d318ba7b
Louise Eriksson <epost.louise@gmail.com>
2020-05-15 16:20:41 +0200

    followed the codealong

---
+++ src/components/Products.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Product } from './Product'
+
+export const Products = () => {
+
+  const allProducts = useSelector((store) => (store.products))
+
+  return (
+    <div className='products'>
+      {allProducts.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
