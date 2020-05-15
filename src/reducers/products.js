241859a108dca1f68fdbe3a215ff2847a133f63f
Louise Eriksson <epost.louise@gmail.com>
2020-05-15 16:41:37 +0200

    added css

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -6,6 +6,11 @@ const productData = [
   { id: 3, title: 'Pants', price: 300, emoji: '👖' },
   { id: 4, title: 'Dress', price: 400, emoji: '👗' },
   { id: 5, title: 'Shoes', price: 500, emoji: '👢' },
+  { id: 6, title: 'Socks', price: 100, emoji: '🧦' },
+  { id: 7, title: 'T-shirt', price: 200, emoji: '👕' },
+  { id: 8, title: 'Pants', price: 300, emoji: '👖' },
+  { id: 9, title: 'Dress', price: 400, emoji: '👗' },
+  { id: 10, title: 'Shoes', price: 500, emoji: '👢' },
 ]
 
 export const products = createSlice({
